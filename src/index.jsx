b72bb772944b33646a37dc21b26fcec96dda15d7
Pedro Fernandes <130870603+PedroFernandes2000@users.noreply.github.com>
2024-12-01 11:07:29 -0300

    bug25

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 
-// Transpilar o JSX manualmente para React.createElement
+
+// Renderiza o componente principal
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  React.createElement(
-    React.StrictMode,
-    null,
-    React.createElement(App, null)
-  )
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
