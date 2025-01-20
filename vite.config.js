e67962627c2aab66471d5aecf5454c8e13f9ca42
Pedro Fernandes <130870603+PedroFernandes2000@users.noreply.github.com>
2025-01-20 20:16:58 -0300

    site refeito

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// Configuração para GitHub Pages
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // Subdiretório do repositório no GitHub Pages
-});
